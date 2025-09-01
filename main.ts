a8f2543e0bdc5bd8328c219aca0374dbad467606
DicrimJac <j.liss.zamora@gmail.com>
2025-08-31 20:59:29 -0400

    primer commit

--- main.ts
+++ main.ts
@@ -1,5 +1,16 @@
-import { serveFile } from "jsr:@std/http/file-server";
+// import { serveFile } from "jsr:@std/http/file-server";
+
+// Deno.serve((req: Request) => {
+//     return serveFile(req, "./index.html");
+// });
+
+import { serveDir } from "jsr:@std/http/file-server";
 
 Deno.serve((req: Request) => {
-    return serveFile(req, "./index.html");
+  return serveDir(req, {
+    fsRoot: "static",   // carpeta raíz que quieres servir (puedes poner "./public" si tienes una carpeta específica)
+    urlRoot: "",   // prefijo de URL ("" = raíz del servidor)
+    showDirListing: false, // muestra listado de directorios si no hay index.html
+    enableCors: true,     // útil si quieres probar desde frontend
+  });
 });
